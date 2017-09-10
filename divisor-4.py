1851289dbd29d147e04aeb9773845baf5f00a202
miwiwa <miwiwa@yahoo.com>
2017-09-10 16:43:04 -0700

    delete duplicates

--- divisor-4.py
+++
@@ -1,8 +0,0 @@
-userNum = input("Please enter a number: ")
-
-a = range(1, int(userNum) + 1)
-b = []
-
-[b.append(num) for num in a if int(userNum) % num == 0]
-
-print(b)
--- oddeven-2.py
+++
@@ -1,8 +0,0 @@
-userNum = input("Please enter a number: ")
-
-if int(userNum) % 4 == 0:
-    print(userNum + " is a multiple of 4")
-elif int(userNum) % 2 == 0:
-    print(userNum + " is an even number")
-else:
-    print(userNum + " is an odd number")
